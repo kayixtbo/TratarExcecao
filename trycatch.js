206165b10c80c81e774babf14bfaa5d68673edd1
Kaylany Luiza Vicente Oliveira <140958751+kayixtbo@users.noreply.github.com>
2023-10-14 02:23:08 +0000

    Finalizando

--- trycatch.js
+++ trycatch.js
@@ -7,13 +7,13 @@ do {
     try {
         number1 = readline.questionInt("\ninforme a primeira nota: ")
         if (number1<0||number1>10) {
-            throw new Error ("\nnúmero inválido.")
+            throw new Error ("\nnúmero inválido. Informe um valor entre 0 e 10")
         }
     } catch (error) {
         console.error(error.message);
         test = true
     } finally {
-        console.log("\nAtualizando...");
+        console.log("\nAtualizando informações...");
     }
 
 } while (test);
@@ -29,9 +29,9 @@ do {
         console.error(error.message);
         test = true
     } finally {
-        console.log("\nAtualizando...");
+        console.log("\nAtualizando informações...");
     }
 
 } while (test);
 average = (number1+number2)/2
-console.log(`\nA média é igual a ${average}`);
+console.log(`\nA média entre ${number1} e ${number2}é igual a ${average.toFixed(2)}`);
